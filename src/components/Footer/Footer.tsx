f964b809e3ec5becedfb3f0e0a74dc28256be478
GoldenLines <thelegioncopywriter@gmail.com>
2023-10-24 18:53:37 -0700

    added footer for porfolio

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,12 @@
+import "../../components/component.css"
+
 export default function Footer() {
     return (
         <div className="footer">
-            Footer
+            <div className="footertexts">
+                <div className="footertext1">SS Engineers INC</div> 
+                <div className="footertext2">Conversion Developers LLC</div>
+            </div>
         </div>
     );
 }
