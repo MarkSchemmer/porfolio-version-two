4f0f99fe53821e9046812ddc2308f6a5d861444b
GoldenLines <thelegioncopywriter@gmail.com>
2023-11-25 18:20:04 -0800

    latest work I'm a silly front-end devleoper.

--- src/components/Body/body.tsx
+++ src/components/Body/body.tsx
@@ -2,6 +2,13 @@ import "../../components/component.css"
 
 export default function Body() {
     return (
-       <div className="body"> </div>
+       <div className="body"> 
+        <section className="section1">
+        <h1>Hi, I'm Mark Welcome to my Page</h1>
+        </section>
+        <section>
+
+        </section>
+       </div>  
     );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,51 +1,37 @@
 import React from 'react';
-import hpv2 from '../../Assets/homepage-backgroundv2.svg';
-import lbe from '../../Assets/top-leftbar-element.svg';
 
-function MyComponent() {
-    return (
-      <div>
-        <img src={hpv2} alt="home-page-image" className='home-page' />
-      </div>
-    );
-  }
+// function component1(){
+//   return (
+//   
+//   );
+// }
 
-  function MyComponent2() {
-    return (
-      <div>
-        <img src={lbe} alt="left-bar-element" className='leftbar-element' />
-      </div>
-    );
-  }
+// function component2(){
+//   return (
+//   
+//   );
+// }
+
+
+// function component3(){
+//   return (
+//   
+//   );
+// }
+
+
+
 
 export default function Header() {
-    return (
-    <div className="header">
-        <div className="topbar">
-
-            <div className="topbar-textbox-right">
-                <div className="right-corner-text">
-                    <div className="about-me">About Me</div>
-                    <div className="contacts">Contacts</div>
-                    <div className="projects">Projects</div>
-                    <div className="codewars">Codewars</div>
-               </div>
-            </div>
-            </div>
-
-              <div className="topbar-leftcorner-insert-box">
-                <div className="left-corner-insert-element">
-                  <div className="ib">
-                 <MyComponent2/>
-                 </div>
-               </div>
-             </div>
-
-              <div className="homepage">
-              <div className="hp">
-              <MyComponent/>
-             </div>
-            </div>
-    </div>
-    );
+return (
+<div className="container">
+  <h3 className='logo'>Mark</h3>
+<nav className="navbar">
+  <a href="/">Home</a>
+  <a href="/">About</a>
+  <a href="/">Contact</a>
+  <a href="/">Projects</a>
+</nav>
+</div>
+);
 }
