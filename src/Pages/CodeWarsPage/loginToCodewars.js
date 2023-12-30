1a91d84dbf53ac37daa3de7d01fba00face5797a
Saku Hasu <mjs.schemmer@gmail.com>
2023-12-30 02:11:28 -0800

    adding back in handleSolution

--- src/Pages/CodeWarsPage/loginToCodewars.js
+++ src/Pages/CodeWarsPage/loginToCodewars.js
@@ -1,32 +1,5 @@
 const { Builder, By, Key } = require('selenium-webdriver');
 
-let solutions = [ ];
-
-// Usage:
-const credentials = {
-    username: "Saku-Hasu",
-    password: "Se*1293#",
-    email: "mjs.schemmer@gmail.com"
-};
-
-// https://www.codewars.com/users/Saku-Hasu/completed_solutions
-const urls = {
-    completedKatasUrl: `https://www.codewars.com/api/v1/users/${credentials.username}/code-challenges/completed`,
-    kataInfoUrl: "https://www.codewars.com/api/v1/code-challenges/",
-    mainFolderPath: "./Katas"
-};
-
-// Function to sign in to CodeWars
-async function signIn(driver, credentials) {
-    await driver.get('https://www.codewars.com/users/sign_in');
-    await driver.findElement(By.id('user_email')).sendKeys(credentials.email);
-    await driver.findElement(By.id('user_password')).sendKeys(credentials.password, Key.RETURN);
-    await driver.wait(async () => {
-        const url = await driver.getCurrentUrl();
-        return url === 'https://www.codewars.com/dashboard';
-    });
-};
-
 async function getNumberOfPagesAndKatas(url) {
     let jdata = await fetch(url);
     let data = await jdata.json();
@@ -41,6 +14,53 @@ async function getKataObject(page) {
     let jdata = await fetch(url);
     let data = await jdata.json();
     return data;
+};
+
+// Usage:
+const credentials = {
+    username: "Saku-Hasu",
+    password: "Se*1293#",
+    email: "mjs.schemmer@gmail.com"
+};
+
+const urls = {
+    completedKatasUrl: `https://www.codewars.com/api/v1/users/${credentials.username}/code-challenges/completed`,
+    kataInfoUrl: "https://www.codewars.com/api/v1/code-challenges/",
+    mainFolderPath: "./Katas"
+};
+
+async function signIn(driver, credentials) {
+    await driver.get('https://www.codewars.com/users/sign_in');
+    await driver.findElement(By.id('user_email')).sendKeys(credentials.email);
+    await driver.findElement(By.id('user_password')).sendKeys(credentials.password, Key.RETURN);
+    await driver.wait(async () => {
+        const url = await driver.getCurrentUrl();
+        return url === 'https://www.codewars.com/dashboard';
+    });
+};
+
+async function handleSolution(driver, languages, id, kataClass) {
+    for await (const language of languages) {
+        //   add file of solution in completed... 
+       //    await getSolutionText(slug, id, language, driver);
+       //    driver.Navigate().GoToUrl($@"https://www.codewars.com/kata/{id}/solutions/{language}/me/newest");
+       await driver.get(`https://www.codewars.com/kata/${id}/solutions/${language}/me/newest`);
+       /*
+            solutionsList = driver.FindElement(By.Id("solutions_list"));
+            solutionItem = solutionsList.FindElement(By.TagName("div"));
+            solutionCode = solutionItem.FindElement(By.TagName("pre")).Text;
+       */
+        let solutionList = await driver.findElement(By.id("solutions_list"));
+        let solutionItem = await solutionList.findElement(By.tagName("div"));
+        let solutionCode = await driver.wait( async () => await solutionItem.findElement(By.tagName("pre")).getText());
+        console.log(solutionCode)
+        kataClass["solutions"].push({
+            "lanugage": language,
+            solution: solutionCode
+        });
+    };
+
+    return kataClass;
 };
 
 // Main scraping function
@@ -62,10 +82,11 @@ async function scrapeCodeWars(credentials) {
         }
     } finally {
         await driver.quit();
+        console.log(JSON.stringify(solutions));
         let newSolutions = JSON.parse(JSON.stringify(solutions));
         const fs = require("fs");
         fs.writeFileSync("kataSolutions.json", JSON.stringify(newSolutions), "utf-8");
     }
-}
+};
 
 scrapeCodeWars(credentials);
