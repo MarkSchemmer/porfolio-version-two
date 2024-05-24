13ef784c95dd14119e32fd0d7c4e9a67aae12638
Saku Hasu <mjs.schemmer@gmail.com>
2024-05-24 10:48:36 -0700

    writing out chess requirments...  So I have a proper roadmap.

---
+++ src/GameApps/Chess/Chess.tsx
@@ -0,0 +1,48 @@
+
+/*
+    In the game of chess we have a board with squares. 
+    And pieces, the pieces all have rules applied to them
+    And the game functions in a turn base sequence. 
+
+    We have White pieces and Black pieces
+
+    List of piece names: 
+
+    - Pond
+    - Knight
+    - Bishop
+    - Rook
+    - Queen
+    - King
+
+    Special game concerns 
+
+    - En-Pessant
+    - Castling
+    - Check
+    - CheckMate
+    - Each pieces unique movement
+
+    The app can be developed in html-css or in a canvas type scenario. 
+
+    Both solutions function...
+
+
+
+    Steps for the game. 
+
+    1. Create a board that responds and has propper coordinates
+    2. Render pieces and add rules to movement for pieces
+    3. Add logic and detection
+    4. make the board resizable and moveable. 
+    4. investigate ai for chess?
+    5. investigate payment using bitcoin for betting on app?
+*/
+
+export const ChessApp = () => {
+    return (
+        <>
+            Chess app. 
+        </>
+    )
+}
