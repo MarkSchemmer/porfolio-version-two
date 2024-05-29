82e13e859650e64d4c9647c89793bb1b81f32630
Saku Hasu <mjs.schemmer@gmail.com>
2024-05-28 19:39:33 -0700

    added hasMoved property

--- src/GameApps/Chess/pieces/Piece.tsx
+++ src/GameApps/Chess/pieces/Piece.tsx
@@ -1,6 +1,8 @@
 export class Piece {
     public pieceName: string;
     public pieceImageName: string;
+    public hasMoved: boolean = false;
+    
     constructor(pieceName: string, pieceImageName: string) {
         this.pieceName = pieceName;
         this.pieceImageName = pieceImageName;
